1fd9080cedf7fb79c77ec5baad4071c8b59c66dc
田中陽祐 <lo.sa.wo.10969.already@gmail.com>
2019-07-09 23:06:17 +0900

    conplete analyze in the case of input is directory

--- jack-analyzer/src/JackAnalyzer.java
+++ jack-analyzer/src/JackAnalyzer.java
@@ -6,72 +6,27 @@ import modules.CompilationEngine;
 import modules.JackTokenizer;
 import modules.data.Keyword;
 
-// 実行コマンド ex. $ JackAnalyzer source
-
-// TODO 次回これやる。ファイル生成先をsourceディレクトリ、もしくはsourceファイルが格納されているディレクトリにする。
-
-/** セットアップや他モジュールの呼び出しを行うモジュール */
+/**
+ * セットアップや他モジュールの呼び出しを行うモジュール<br>
+ * 実行コマンド ex. $ JackAnalyzer source
+ */
 public class JackAnalyzer {
   public static void main(String[] args) {
 
     var source = new File(args[0]);
-    String jackProgram = JackAnalyzer.readAllProgramInJackfile(source);
 
     if (source.isDirectory()) {
-      // TODO 上のやつが終わったらこれやる。sourceがディレクトリの時の処理
-      // .jackファイルでフィルターをかけて.jackファイルの配列を作る。
-      // file を one by one でコンパイル処理をする。
-      // トークナイザが出力するファイルも構文木もは.jackにつき1つ
-
-      // TODO 出力先をこのディレクトリにする。source.getName()+[file_name]T.xml, .xml
+      File[] jackFiles = source.listFiles();
+      for (var jackFile : jackFiles) {
+        if (jackFile.getName().endsWith(".jack")) {
+          analyze(jackFile);
+        }
+      }
     } else if (source.isFile()) {
-
       if (!source.getName().endsWith(".jack")) {
         throw new IllegalArgumentException("指定されたファイルは.jackファイルではありません。");
       }
-
-      int lastDotIndexOfInputFile = source.getName().lastIndexOf(".");
-      String coreName = source.getName().substring(0, lastDotIndexOfInputFile);
-      String tokenizerOutputFilename = source.getParent() + "/" + coreName + "T.xml";
-
-      try (var jackTokenizer = new JackTokenizer(tokenizerOutputFilename, jackProgram.toString())) {
-
-        // ⑵ Xxx.xml という名前の出力ファイルを作り、それに書き込みを行う準備をする。
-        String compileEngineOutputFilename = source.getParent() + "/" + coreName + ".xml";
-        try (var comlilationEngine = new CompilationEngine(source, compileEngineOutputFilename)) {
-          // ⑶ 入力である JackTokenizer を出力ファイルへコンパイルするために、ConpilationEngine を用いる。
-        }
-
-        jackTokenizer.advance();
-        while (true) {
-          switch (jackTokenizer.tokenType()) {
-            case KEYWORD:
-              Keyword keyword = jackTokenizer.keyword();
-              break;
-            case SYMBOL:
-              char symbol = jackTokenizer.symbol();
-              break;
-            case INT_CONST:
-              int intConst = jackTokenizer.intVal();
-              break;
-            case IDENTIFIER:
-              String indentifier = jackTokenizer.identifier();
-              break;
-            case STRING_CONST:
-              String stringConst = jackTokenizer.stringVal();
-              break;
-          }
-          if (jackTokenizer.hasMoreTokens() == true) {
-            jackTokenizer.advance();
-          } else {
-            break;
-          }
-        }
-
-      } catch (IOException e) {
-        System.out.println("ファイルを開けませんでした。");
-        System.out.println(e.getMessage());
-      }
+      analyze(source);
     }
   }
 
@@ -105,5 +60,53 @@ public class JackAnalyzer {
       System.exit(1);
     }
     return readString.toString();
+  }
+
+  private static void analyze(File source) {
+    String jackProgram = JackAnalyzer.readAllProgramInJackfile(source);
+
+    int lastDotIndexOfInputFile = source.getName().lastIndexOf(".");
+    String coreName = source.getName().substring(0, lastDotIndexOfInputFile);
+    String tokenizerOutputFilename = source.getParent() + "/" + coreName + "T.xml";
+    String compileEngineOutputFilename = source.getParent() + "/" + coreName + ".xml";
+
+    try (var jackTokenizer = new JackTokenizer(tokenizerOutputFilename, jackProgram)) {
+
+      // ⑵ Xxx.xml という名前の出力ファイルを作り、それに書き込みを行う準備をする。
+      try (var comlilationEngine = new CompilationEngine(source, compileEngineOutputFilename)) {
+        // ⑶ 入力である JackTokenizer を出力ファイルへコンパイルするために、ConpilationEngine を用いる。
+        // TODO トークナイザの処理をこの中に入れる。
+      }
+
+      jackTokenizer.advance();
+      while (true) {
+        switch (jackTokenizer.tokenType()) {
+          case KEYWORD:
+            Keyword keyword = jackTokenizer.keyword();
+            break;
+          case SYMBOL:
+            char symbol = jackTokenizer.symbol();
+            break;
+          case INT_CONST:
+            int intConst = jackTokenizer.intVal();
+            break;
+          case IDENTIFIER:
+            String indentifier = jackTokenizer.identifier();
+            break;
+          case STRING_CONST:
+            String stringConst = jackTokenizer.stringVal();
+            break;
+        }
+        if (jackTokenizer.hasMoreTokens() == true) {
+          jackTokenizer.advance();
+        } else {
+          break;
+        }
+      }
+
+    } catch (IOException e) {
+      System.out.println("ファイルを開けませんでした。");
+      System.out.println(e.getMessage());
+    }
   }
 }
