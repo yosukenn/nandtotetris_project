565083e59ecea9d9f9b2bf138c7ad906428ba769
田中陽祐 <lo.sa.wo.10969.already@gmail.com>
2019-07-16 00:09:02 +0900

    user DOM API for creating XML file

--- jack-analyzer/src/modules/CompilationEngine.java
+++ jack-analyzer/src/modules/CompilationEngine.java
@@ -1,40 +1,84 @@
 package modules;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileInputStream;
+import java.io.File;
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
-import javax.xml.stream.XMLInputFactory;
-import javax.xml.stream.XMLStreamConstants;
-import javax.xml.stream.XMLStreamException;
-import javax.xml.stream.XMLStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import org.w3c.dom.Document;
 
 /** 再帰によるトップダウン式の解析器<br> */
 public class CompilationEngine implements AutoCloseable {
 
   BufferedReader reader;
-  BufferedWriter writer;
+
+  // TODO 書き込みはそういうAPIを使ったほうがいい。
+  // 改行とか https://teratail.com/questions/13471
+  DocumentBuilder documentBuilder = null;
+  Document document;
+
+  private File outputFile;
+
+  private static final String ELEMENT_TYPE = "elementType";
+  private static final String CONTENT = "content";
 
   public CompilationEngine(String inputFile, String outputFile) throws IOException {
     this.reader = new BufferedReader(new FileReader(inputFile));
-    this.writer = new BufferedWriter(new FileWriter(outputFile));
+    var firstLine = this.reader.readLine();
+    if (!firstLine.equals("<tokens>")) {
+      throw new IllegalArgumentException("入力ファイルの形式が正しくありません。firstLine=" + firstLine);
+    }
+
+    try {
+      this.documentBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+    } catch (ParserConfigurationException e) {
+      e.printStackTrace();
+    }
+    document = documentBuilder.newDocument();
+    document.setXmlStandalone(true);
+
+    this.outputFile = new File(outputFile);
   }
 
   @Override
-  public void close() throws IOException, XMLStreamException {
+  public void close() throws IOException {
     this.reader.close();
-    this.writer.close();
+    createXMLFile(this.outputFile, this.document);
   }
 
-  // TODO まずは書き込みは考えずに読み込みだけ実装。
-  public void compileClass() throws XMLStreamException {
-    // 字句要素 keyword の class でなかったら例外。
+  public void compileClass() throws IOException {
+    var firstLine = parseXmlLine(this.reader.readLine());
+    if (!firstLine.get(CONTENT).equals("class")) {
+      throw new IllegalStateException();
+    }
 
-    // <class></class> の書き出し
+    // classを書き込む
 
     // xml要素の種類によって適切な処理を呼び出す。
+  }
+
+  private Map<String, String> parseXmlLine(String line) {
+    var map = new HashMap<String, String>();
+
+    String regex = "(<\\w+>) (\\w+) (</\\w+>)";
+    Pattern p = Pattern.compile(regex);
+    Matcher m = p.matcher(line);
+
+    map.put(ELEMENT_TYPE, m.group(1).substring(1, m.group(1).length() - 1));
+    map.put(CONTENT, m.group(2));
+    return map;
   }
 
   public void compileClassVarDec() {}
@@ -62,4 +106,33 @@ public class CompilationEngine implements AutoCloseable {
   public void compileTerm() {}
 
   public void compileExpressionList() {}
+
+  private static boolean createXMLFile(File file, Document document) {
+
+    // Transformerインスタンスの生成
+    Transformer transformer = null;
+    try {
+      TransformerFactory transformerFactory = TransformerFactory.newInstance();
+      transformer = transformerFactory.newTransformer();
+    } catch (TransformerConfigurationException e) {
+      e.printStackTrace();
+      return false;
+    }
+
+    // Transformerの設定
+    transformer.setOutputProperty("indent", "yes"); // 改行指定
+    transformer.setOutputProperty("encoding", "UTF-8"); // エンコーディング
+    transformer.setOutputProperty("{http://xml.apache.org/xalan}indent-amount", "2"); // インデントの桁指定
+    transformer.setOutputProperty("method", "html"); // XML宣言部分を省略
+
+    // XMLファイルの作成
+    try {
+      transformer.transform(new DOMSource(document), new StreamResult(file));
+    } catch (TransformerException e) {
+      e.printStackTrace();
+      return false;
+    }
+
+    return true;
+  }
 }
